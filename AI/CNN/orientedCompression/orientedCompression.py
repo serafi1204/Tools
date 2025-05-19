eaa2fde2b4ff253a8c19283484cf6718cd0d6c38
serafi1204 <122208990+serafi1204@users.noreply.github.com>
2025-05-19 13:24:37 +0900

    Create orientedCompression.py

---
+++ AI/CNN/orientedCompression/orientedCompression.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+class orientedCompression():
+    def __init__(self, model:nn.Module, *filter):
+        self.model = model
+        self.dic = self.getParamMap(model, *filter)
+
+    def getParamMap(model:nn.Module, *filters):
+        freeze_weights = []
+        
+        for name, module in model.named_modules():
+            if (not isinstance(module, nn.Conv2d)): continue
+            
+            cnt = 0
+            weight = module.weight 
+            for out_ch in range(weight.shape[0]):
+                for in_ch in range(weight.shape[1]):
+                    for i in range(weight.shape[2]):
+                        for j in range(weight.shape[3]):
+                            key = [out_ch, in_ch, i, j]
+                            val = weight[*key].item()
+                            
+                            for func in filters:
+                                if (not func(val)): break
+                            else:
+                                freeze_weights.append([key, val])
+                                cnt += 1
+            
+            print(f"Conv Layer: {name}, Weight shape: {weight.shape} / selected params: {cnt}")
+        
+        return freeze_weights
+    
+    def backward(self):
+        with torch.no_grad():
+            for (module, out_ch, in_ch, i, j), val in self.dic:
+                module.weight[out_ch, in_ch, i, j] = val
