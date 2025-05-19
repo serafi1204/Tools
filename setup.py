5e070c46c3be458cb92eceee1767473308eddc2b
serafi1204 <122208990+serafi1204@users.noreply.github.com>
2025-05-19 13:35:30 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+  name='tools',
+  version='0.0.1',
+  description='install libs',
+  url = 'https://github.com/serafi1204/Tools.git',
+  auther='None',
+  auther='None',
+  install_requires=[
+    
+  ]
+)
