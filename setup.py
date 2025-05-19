3541a22c5a85fcba4cf3de86887de1cf887fd3ff
serafi1204 <122208990+serafi1204@users.noreply.github.com>
2025-05-19 13:51:33 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='orientedCompression',
+    name='Tools',  # 패키지명
     version='0.1.0',
     author='None',
     author_email='None',
     description='PyTorch utility to freeze selected Conv2d kernel weights during training',
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
-    py_modules=['orientedCompression'],
+    packages=find_packages(),  # tools 폴더를 패키지로 포함
+    package_dir={'': '.'},
+    include_package_data=True,
     install_requires=[
-        'torch>=1.7.0'
+        'torch>=1.7.0',
     ],
     python_requires='>=3.7',
     classifiers=[
